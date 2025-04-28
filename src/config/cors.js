757dcf880b14a85e130a45cfb0b9a25be3dc8fa7
gulsherkooner <gulsher.kooner@gmail.com>
2025-04-28 15:44:50 +0530

    .

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,10 +1,10 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://api-gateway-sooty-nine.vercel.app',
+  origin: ['https://api-gateway-sooty-nine.vercel.app', 'http://localhost:3001'],
   credentials: true, // Allow cookies (e.g., refreshToken)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],
 };
 
 module.exports = cors(corsOptions);

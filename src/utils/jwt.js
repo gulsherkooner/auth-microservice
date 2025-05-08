4f44f21c5aacc3a7710ffc91e2b1255c8fbf4ab7
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-08 17:44:24 +0530

    updated jwt time of expiry

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const generateAccessToken = (user) => {
-  return jwt.sign({ user_id: user.user_id }, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+  return jwt.sign({ user_id: user.user_id }, process.env.JWT_ACCESS_SECRET, { expiresIn: '1d' });
 };
 
 const generateRefreshToken = (user) => {

35ef3a0b0b27f5a6410de34788744fe783e85996
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-30 09:31:44 +0530

    .

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['https://api-gateway-eta-navy.vercel.app', 'http://135.181.192.55:3001','http://135.181.192.55:3005'],
+  origin: ['http://api-gateway:3001','http://post-service:3005'],
   credentials: true, // Allow cookies (e.g., refreshToken)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],

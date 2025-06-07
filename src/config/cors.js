ccedbac8207997d2502481c0e2b9c2fd5b7e4ae5
gulsherkooner <gulsher.kooner@gmail.com>
2025-06-07 11:54:54 +0530

    updated comments in posts

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://api-gateway:3001','http://post-service:3005'],
+  origin: ['http://api-gateway:3001','http://post-service:3005', "http://localhost:3001", "http://localhost:3005"],
   credentials: true, // Allow cookies (e.g., refreshToken)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],
